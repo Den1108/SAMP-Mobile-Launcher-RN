56844b464b863f08ce21755c9632ed03adb57129
Den1 <zeromen945@gmail.com>
2026-03-11 15:16:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ import { Provider } from 'react-redux';
 import { App } from './App';
 import { name as appName } from './app.json';
 import { store } from './src/store/store';
+import { FileLogger } from 'react-native-file-logger';
+
+FileLogger.configure({
+  captureConsole: true,        // все console.log будут писаться в файл
+  dailyRolling: true,          // новый файл каждый день
+  maximumFileSize: 1024 * 1024, // 1 МБ на файл
+  maximumNumberOfFiles: 5      // хранить до 5 файлов
+}).then(() => {
+  console.log('✅ File logger initialized');
+}).catch(err => {
+  console.log('❌ Failed to init logger:', err);
+});
 
 const RNRedux = () => (
   <Provider store={store}>
